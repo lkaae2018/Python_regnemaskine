8f62c89684f804b53683f29d90584a0de9fae73c
lkaae2018 <lak@djhhadsten.dk>
2019-05-01 07:27:15 +0200

    Initial commit

---
+++ regnemaskine.py
@@ -0,0 +1,41 @@
+#Regnemaskine
+
+def Plus(x,y):
+    resultat =x+y
+    return resultat
+
+def Minus(x,y):
+
+def Gange(x,y):
+
+
+def Divider(x,y):
+
+
+def menu():
+    print("Tast 1(+), 2(-), 3(*), 4(/), 5(slut)")
+
+menu()
+Menutal = int(input("tast tal nu!!"))
+
+while Menutal==1:
+    print("Nu skal der lægges sammen(+)!")
+    #Indtast tal1 som floatingpoint tal!
+    #Indtast tal2 som floatingpoint tal!
+    print("Resultatet af",tal1,"+",tal2,"=",Plus(tal1, tal2))
+    print()
+    menu()
+    Menutal = int(input("Tast tal nu!!"))
+
+while Menutal==2:
+
+
+while Menutal==3:
+
+
+while Menutal==4:
+
+
+while Menutal==5:
+    print("Slut!!!!!!!")
+    break
